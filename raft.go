5e4f92e60de2028d62fa3de2048c7a6aa99a482f
jh.song <jh.song@openbayes.com>
2025-08-07 01:38:30 +0800

    feat: init raft struct

--- raft.go
+++ raft.go
@@ -1,5 +1,7 @@
 package raft
 
+import "errors"
+
 // Possible values for StateType.
 const (
 	StateFollower StateType = iota
@@ -18,4 +20,56 @@ var stmap = [...]string{
 
 func (st StateType) String() string {
 	return stmap[st]
+}
+
+// Config contains the parameters to start a raft.
+type Config struct {
+	// ID is the identity of the local raft. ID cannot be 0.
+	ID uint64
+
+	// ElectionTick is the number of Node.Tick invocations that must pass between
+	// elections. That is, if a follower does not receive any message from the
+	// leader of current term before ElectionTick has elapsed, it will become
+	// candidate and start an election. ElectionTick must be greater than
+	// HeartbeatTick. We suggest ElectionTick = 10 * HeartbeatTick to avoid
+	// unnecessary leader switching.
+	ElectionTick int
+	// HeartbeatTick is the number of Node.Tick invocations that must pass between
+	// heartbeats. That is, a leader sends heartbeat messages to maintain its
+	// leadership every HeartbeatTick ticks.
+	HeartbeatTick int
+}
+
+func (c *Config) validate() error {
+	if c.HeartbeatTick <= 0 {
+		return errors.New("heartbeat tick must be greater than 0")
+	}
+
+	if c.ElectionTick <= c.HeartbeatTick {
+		return errors.New("election tick must be greater than heartbeat tick")
+	}
+
+	return nil
+}
+
+type raft struct {
+	id uint64
+
+	state StateType
+
+	heartbeatTimeout int
+	electionTimeout  int
+}
+
+func newRaft(c *Config) *raft {
+	if err := c.validate(); err != nil {
+		panic(err.Error())
+	}
+
+	r := &raft{
+		id:               c.ID,
+		electionTimeout:  c.ElectionTick,
+		heartbeatTimeout: c.HeartbeatTick,
+	}
+	return r
 }
